cf9727cbd4afd84d82803206dff45768aa2ef25c
Julia van Driel <0945409@hr.nl>
2019-09-23 17:08:08 +0200

    enter

--- Week3/src/main/java/com/example/Week3/Week3Application.java
+++ Week3/src/main/java/com/example/Week3/Week3Application.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Week3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Week3Application.class, args);
+	public static void main(String[] args2) {
+		SpringApplication.run(Week3Application.class, args2);
 	}
+
+
+
+
+
 
 }
